3bc8f45509c0c6a790ebf98895a97ad775c24bb6
Peter Weiser <peterweiser87@gmail.com>
2024-02-06 21:39:04 +0100

    separated server and client in hero section

---
+++ src/components/hero/ClientComponent.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FC } from "react";
+import CountUpNumber from "../count-up-number/CountUpNumber";
+
+type Props = {
+  heading: React.ReactNode;
+  images: React.ReactNode;
+};
+
+const ClientComponent: FC<Props> = (props) => {
+  const { heading, images } = props;
+
+  return (
+    <section className="flex px-4 items-center xl:gap-20 gap-12 md:container mx-auto">
+      <div className="h-full">
+        {heading}
+
+        <div className="flex justify-between mt-16">
+          <div className="fleet-container">
+            <p className="fleet-title">Airplanes</p>
+            <CountUpNumber duration={1000} endValue={14} />
+          </div>
+          <div className="fleet-container">
+            <p className="fleet-title">Helicopters</p>
+            <CountUpNumber duration={1000} endValue={8} />
+          </div>
+          <div className="fleet-container">
+            <p className="fleet-title">Balloons</p>
+            <CountUpNumber duration={1000} endValue={12} />
+          </div>
+        </div>
+      </div>
+
+      {images}
+    </section>
+  );
+};
+
+export default ClientComponent;
---
+++ src/components/hero/ServerComponent.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+export const heading = (
+  <>
+    <h1 className="font-heading mb-6 ">Explore Our Fleet</h1>
+    <p className="text-textdark dark:text-textlight mb-8 max-w-lg font-normal">
+      Elevate your senses and discover Hungary's enchanting beauty from the sky.
+    </p>
+    <button className="btn-primary">Get Started</button>
+  </>
+);
+
+export const images = (
+  <div className="md:grid hidden gap-8 grid-cols-1">
+    <div className="rounded-2xl overflow-hidden h-54">
+      <Image
+        src="/images/balloon_04.webp"
+        alt="hero-1"
+        width={300}
+        height={300}
+        className="img scale-anim"
+      />
+    </div>
+
+    <div className="grid grid-cols-2 gap-8 h-48">
+      <div className="rounded-2xl overflow-hidden">
+        <Image
+          src="/images/heli_03.webp"
+          alt="hero-2"
+          width={300}
+          height={300}
+          className="img scale-anim"
+        />
+      </div>
+
+      <div className="rounded-2xl overflow-hidden">
+        <Image
+          src="/images/plane_01.webp"
+          alt="hero-3"
+          width={300}
+          height={300}
+          className="img scale-anim"
+        />
+      </div>
+    </div>
+  </div>
+);
